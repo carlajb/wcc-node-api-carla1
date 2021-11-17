8746010def42018b6bd32dd455cce08558180034
Carla Janaina Braga <carla_agencia@hotmail.com>
2021-11-16 21:39:31 -0300

    minhas primeiras rotas

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require ("express");
 const app = express();
 const port = 8080;
 
+//GET- obter algo
+//POST -criar algo
+// PUT - atualizar algo
+// DELETE - deletar algo
+// npm install --save -dev nodemon
+
+app.use(express.json());
+
 app.get("/", function(req,res){
     res.send("minha primeirac requisicao");
 });
@@ -13,6 +21,22 @@ app.get("/segunda-req", function(req,res){
 app.get("/com-param", function(req,res){
     res.send("requisicao com param!!! " + req.query.nome);
 });
+
+app.post("/meu-primeiro-post", function (req,res){
+    console.log(req.body);
+    res.send("Meu post funcionou");
+});
+
+app.put("/meu-primeiro-put/:id", function (req, res){
+       console.log(req.body, req.params.id);
+        res.send("meu put funciona");
+});
+
+app.delete("/meu-primeiro-delete/:id", function (req,res){
+    console.log(req.params.id);
+    res.send("Meu delete funciona" + req.params.id);
+});
+
 
 
 app.listen(port, function(){
