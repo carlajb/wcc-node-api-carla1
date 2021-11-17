e034bf1ec0c47464676d10fb360bc25ac9cdf507
Carla Janaina Braga <carla_agencia@hotmail.com>
2021-11-16 21:55:38 -0300

    criacao de novas pastas

--- index.js
+++ index.js
@@ -11,35 +11,11 @@ const port = 8080;
 app.use(express.json());
 
 app.get("/", function(req,res){
-    res.send("minha primeirac requisicao");
+    res.send("Dasa Educa - Artigos");
 });
-
-app.get("/segunda-req", function(req,res){
-    res.send("minha segunda requisicao");
-});
-
-app.get("/com-param", function(req,res){
-    res.send("requisicao com param!!! " + req.query.nome);
-});
-
-app.post("/meu-primeiro-post", function (req,res){
-    console.log(req.body);
-    res.send("Meu post funcionou");
-});
-
-app.put("/meu-primeiro-put/:id", function (req, res){
-       console.log(req.body, req.params.id);
-        res.send("meu put funciona");
-});
-
-app.delete("/meu-primeiro-delete/:id", function (req,res){
-    console.log(req.params.id);
-    res.send("Meu delete funciona" + req.params.id);
-});
-
 
 
 app.listen(port, function(){
-    console.log("ouvindo a porta 8080", port);
+    console.log("ouvindo a porta", port);
 });
 
---
+++ routes/artigos.routes.js
@@ -0,0 +1,23 @@
+//rotas de sistemas de artigos 
+
+/*
+GET
+
+-obter todos os artigos 
+-obter um artigo especifico
+-obter todos os artigos publicos 
+
+POST
+ 
+-criar um novo artigo
+
+PUT
+
+-publicar meu artigo 
+
+DELETE
+
+-deletar um artigo 
+
+
+*/
